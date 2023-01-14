8678f4796d53ca47e9840d60dbf71494fc583187
alikashmar8 <alikashmar8@gmail.com>
2023-01-14 16:01:33 +0200

    update imports

--- src/service-types/service-types.module.ts
+++ src/service-types/service-types.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
 import { EmployeesService } from 'src/employees/employees.service';
 import { Employee } from 'src/employees/entities/employee.entity';
+import { Setting } from 'src/settings/entities/setting.entity';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { ServiceType } from './entities/service-type.entity';
@@ -10,7 +11,15 @@ import { ServiceTypesController } from './service-types.controller';
 import { ServiceTypesService } from './service-types.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ServiceType, User, DeviceToken, Employee])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ServiceType,
+      User,
+      DeviceToken,
+      Employee,
+      Setting,
+    ]),
+  ],
   controllers: [ServiceTypesController],
   providers: [ServiceTypesService, UsersService, EmployeesService],
 })
