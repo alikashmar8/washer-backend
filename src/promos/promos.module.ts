381043106ef64dab3655c17db4f7f26be4bd0ce8
alikashmar8 <alikashmar8@gmail.com>
2023-09-04 20:29:04 +0300

    update promos module

--- src/promos/promos.module.ts
+++ src/promos/promos.module.ts
@@ -3,18 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from 'src/app.service';
 import { BranchesService } from 'src/branches/branches.service';
 import { Branch } from 'src/branches/entities/branch.entity';
+import { ChatsService } from 'src/chats/chats.service';
 import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/chats/entities/message.entity';
 import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
 import { EmployeesService } from 'src/employees/employees.service';
 import { Employee } from 'src/employees/entities/employee.entity';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { NotificationsService } from 'src/notifications/notifications.service';
 import { Setting } from 'src/settings/entities/setting.entity';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { Promo } from './entities/promo.entity';
 import { PromosController } from './promos.controller';
 import { PromosService } from './promos.service';
-import { ChatsService } from 'src/chats/chats.service';
-import { Message } from 'src/chats/entities/message.entity';
 
 @Module({
   imports: [
@@ -27,6 +29,7 @@ import { Message } from 'src/chats/entities/message.entity';
       User,
       Chat,
       Message,
+      Notification,
     ]),
   ],
   controllers: [PromosController],
@@ -37,6 +40,7 @@ import { Message } from 'src/chats/entities/message.entity';
     BranchesService,
     UsersService,
     ChatsService,
+    NotificationsService,
   ],
 })
 export class PromosModule {}
