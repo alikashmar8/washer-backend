86be1a17da25cefccc659779327fd7f21b6b87e0
alikashmar8 <alikashmar8@gmail.com>
2023-09-05 21:39:39 +0300

    update products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,20 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from 'src/app.service';
 import { BranchesService } from 'src/branches/branches.service';
 import { Branch } from 'src/branches/entities/branch.entity';
+import { ChatsService } from 'src/chats/chats.service';
 import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/chats/entities/message.entity';
 import { DeviceTokensService } from 'src/device-tokens/device-tokens.service';
 import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
 import { EmployeesService } from 'src/employees/employees.service';
 import { Employee } from 'src/employees/entities/employee.entity';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { NotificationsService } from 'src/notifications/notifications.service';
 import { Setting } from 'src/settings/entities/setting.entity';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { ProductImage } from './entities/product-image.entity';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { ProductImage } from './entities/product-image.entity';
-import { ChatsService } from 'src/chats/chats.service';
-import { Message } from 'src/chats/entities/message.entity';
 
 @Module({
   imports: [
@@ -30,6 +32,7 @@ import { Message } from 'src/chats/entities/message.entity';
       ProductImage,
       Chat,
       Message,
+      Notification,
     ]),
   ],
   controllers: [ProductsController],
@@ -41,6 +44,7 @@ import { Message } from 'src/chats/entities/message.entity';
     AppService,
     BranchesService,
     ChatsService,
+    NotificationsService,
   ],
 })
 export class ProductsModule {}
