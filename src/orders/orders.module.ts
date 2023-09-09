578f992e89be4f49e09a7e59d661f21bc469d29a
alikashmar8 <alikashmar8@gmail.com>
2023-09-09 23:46:23 +0300

    update orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from 'src/app.service';
 import { BranchesService } from 'src/branches/branches.service';
 import { Branch } from 'src/branches/entities/branch.entity';
+import { ChatsService } from 'src/chats/chats.service';
 import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/chats/entities/message.entity';
 import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
 import { EmployeesService } from 'src/employees/employees.service';
 import { Employee } from 'src/employees/entities/employee.entity';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { NotificationsService } from 'src/notifications/notifications.service';
 import { Product } from 'src/products/entities/product.entity';
 import { Promo } from 'src/promos/entities/promo.entity';
 import { PromosService } from 'src/promos/promos.service';
@@ -17,8 +21,6 @@ import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/orderItem.entity';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-import { ChatsService } from 'src/chats/chats.service';
-import { Message } from 'src/chats/entities/message.entity';
 
 @Module({
   imports: [
@@ -34,6 +36,7 @@ import { Message } from 'src/chats/entities/message.entity';
       Branch,
       Chat,
       Message,
+      Notification,
     ]),
   ],
   controllers: [OrdersController],
@@ -45,6 +48,7 @@ import { Message } from 'src/chats/entities/message.entity';
     AppService,
     BranchesService,
     ChatsService,
+    NotificationsService,
   ],
 })
 export class OrdersModule {}
