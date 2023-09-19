d49b0b4c201cd734176202b56fb48fcc82e6c0c6
alikashmar8 <alikashmar8@gmail.com>
2023-09-19 21:56:32 +0300

    update employees module

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -3,17 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from 'src/app.service';
 import { BranchesService } from 'src/branches/branches.service';
 import { Branch } from 'src/branches/entities/branch.entity';
+import { ChatsService } from 'src/chats/chats.service';
 import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/chats/entities/message.entity';
 import { DeviceTokensService } from 'src/device-tokens/device-tokens.service';
 import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { NotificationsService } from 'src/notifications/notifications.service';
 import { Setting } from 'src/settings/entities/setting.entity';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
 import { Employee } from './entities/employee.entity';
-import { Message } from 'src/chats/entities/message.entity';
-import { ChatsService } from 'src/chats/chats.service';
 
 @Module({
   imports: [
@@ -25,6 +27,7 @@ import { ChatsService } from 'src/chats/chats.service';
       User,
       Chat,
       Message,
+      Notification,
     ]),
   ],
   controllers: [EmployeesController],
@@ -35,6 +38,7 @@ import { ChatsService } from 'src/chats/chats.service';
     BranchesService,
     UsersService,
     ChatsService,
+    NotificationsService,
   ],
   exports: [EmployeesService],
 })
