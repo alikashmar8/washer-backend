b34cd1970eb07766ad6812c0067ab2554190a14f
alikashmar8 <alikashmar8@gmail.com>
2023-09-28 21:29:33 +0300

    refactor credit cards module

--- src/credit-cards/credit-cards.module.ts
+++ src/credit-cards/credit-cards.module.ts
@@ -1,20 +1,34 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatsService } from 'src/chats/chats.service';
 import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/chats/entities/message.entity';
 import { CreditCard } from 'src/credit-cards/entities/credit-card.entity';
 import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
+import { Notification } from 'src/notifications/entities/notification.entity';
+import { NotificationsService } from 'src/notifications/notifications.service';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { CreditCardsController } from './credit-cards.controller';
 import { CreditCardsService } from './credit-cards.service';
-import { ChatsService } from 'src/chats/chats.service';
-import { Message } from 'src/chats/entities/message.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CreditCard, User, DeviceToken, Chat, Message]),
+    TypeOrmModule.forFeature([
+      CreditCard,
+      User,
+      DeviceToken,
+      Chat,
+      Message,
+      Notification,
+    ]),
   ],
   controllers: [CreditCardsController],
-  providers: [CreditCardsService, UsersService, ChatsService],
+  providers: [
+    CreditCardsService,
+    UsersService,
+    ChatsService,
+    NotificationsService,
+  ],
 })
 export class CreditCardsModule {}
