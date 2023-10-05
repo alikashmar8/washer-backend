88f3cc31af7461b671a5b5ce85cbf1b30409e1d2
alikashmar8 <alikashmar8@gmail.com>
2023-10-05 20:24:36 +0300

    add global message API

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,20 +1,29 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
   Patch,
+  Post,
   Query,
   UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { CurrentEmployee } from 'src/common/decorators/current-employee.decorator';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { EmployeeRole } from 'src/common/enums/employee-role.enum';
 import { Employee } from 'src/employees/entities/employee.entity';
 import { User } from 'src/users/entities/user.entity';
 import { AuthGuard } from './../auth/guards/auth.guard';
 import { ChatsService } from './chats.service';
+import { SendGlobalMessageDTO } from './dto/send-global-message.dto';
 
 @ApiBearerAuth('access_token')
+@UsePipes(new ValidationPipe())
 @ApiTags('Chats')
 @Controller('chats')
 export class ChatsController {
@@ -55,4 +64,15 @@ export class ChatsController {
   // remove(@Param('id') id: string) {
   //   return this.chatsService.remove(+id);
   // }
+
+  @Roles(EmployeeRole.ADMIN)
+  @UseGuards(RolesGuard)
+  @Post('sendGlobalMessage')
+  async sendGlobalMessage(
+    @Body() dto: SendGlobalMessageDTO,
+    @CurrentEmployee() currentEmployee: Employee,
+  ) {
+    this.chatsService.sendGlobalMessage(currentEmployee, dto);
+    return { message: 'Sending message in progress...' };
+  }
 }
