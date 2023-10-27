1a77fd88b93ea0e1cab372c8f0b3d47a5f52cda3
alikashmar8 <alikashmar8@gmail.com>
2023-10-27 19:50:08 +0300

    validate address by branch location

--- src/addresses/addresses.service.ts
+++ src/addresses/addresses.service.ts
@@ -11,6 +11,9 @@ import { EmployeeRole } from './../common/enums/employee-role.enum';
 import { CreateUserAddressDto } from './dto/create-user-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
 import { Address } from './entities/address.entity';
+import { BranchesService } from 'src/branches/branches.service';
+import { calculateDistance } from 'src/common/utils/functions';
+import { Branch } from 'src/branches/entities/branch.entity';
 
 @Injectable()
 export class AddressesService {
@@ -26,9 +29,42 @@ export class AddressesService {
   }
   constructor(
     @InjectRepository(Address) private addressesRepository: Repository<Address>,
+    private branchesService: BranchesService,
   ) {}
 
   async create(createAddressDto: CreateUserAddressDto) {
+    // Validate if the user's address is within any branch's coverage area
+    const res = await this.branchesService.findAll({}, ['address']); // Replace with your actual method to fetch branches
+    const branches = res.data;
+    const userLat = createAddressDto.lat;
+    const userLon = createAddressDto.lon;
+
+    let nearestBranch: Branch;
+    let nearestDistance: number = Number.MAX_VALUE;
+    for (const branch of branches) {
+      const branchLat = branch.address.lat;
+      const branchLon = branch.address.lon;
+
+      const distance = calculateDistance(
+        { lat: userLat, lon: userLon },
+        { lat: branchLat, lon: branchLon },
+      );
+
+      if (distance < nearestDistance) {
+        nearestBranch = branch;
+        nearestDistance = distance;
+      }
+    }
+
+    if (
+      nearestDistance == Number.MAX_VALUE ||
+      !nearestBranch ||
+      nearestBranch.coverageArea < nearestDistance
+    )
+      throw new BadRequestException(
+        'No close branch was found near your address!',
+      );
+
     return await this.addressesRepository.save(createAddressDto);
   }
 
