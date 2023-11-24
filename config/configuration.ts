d41397da6ebe5d30c932ef7b37459dae5097d739
alikashmar8 <alikashmar8@gmail.com>
2023-11-24 22:33:37 +0200

    add google configuration tags

--- config/configuration.ts
+++ config/configuration.ts
@@ -6,7 +6,7 @@ export default () => ({
     host: process.env.TYPEORM_HOST || '127.0.0.1',
     username: process.env.TYPEORM_USERNAME || 'root',
     password: process.env.TYPEORM_PASSWORD || '',
-    database: process.env.TYPEORM_DATABASE || 'washer_db',
+    database: process.env.TYPEORM_DATABASE || 'cleanclinic',
     port: parseInt(process.env.TYPEORM_PORT) || 3306,
     logging: process.env.TYPEORM_LOGGING === 'true',
     entities: [path.join(__dirname, '../**/*.entity{.ts,.js}')],
@@ -29,5 +29,10 @@ export default () => ({
     secure: process.env.MAIL_SECURE,
     user: process.env.MAIL_IGNORE_USER,
     pass: process.env.MAIL_IGNORE_PASS,
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientId2: process.env.GOOGLE_CLIENT_ID2,
+    clientSecret: process.env.GOOGLE_SECRET_KEY,
   },
 });
