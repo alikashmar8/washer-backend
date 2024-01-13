db0d441f40a2d5edd837713ad88984ae75258bf0
alikashmar8 <alikashmar8@gmail.com>
2024-01-13 20:21:28 +0200

    Add balance validation to UpdateWalletDto and update update() method in WalletsService

--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { Wallet } from './entities/wallet.entity';
 
 @Injectable()
 export class WalletsService {
+  constructor(
+    @InjectRepository(Wallet)
+    private readonly walletRepository: Repository<Wallet>,
+  ) {}
   create(createWalletDto: CreateWalletDto) {
     return 'This action adds a new wallet';
   }
@@ -16,8 +23,21 @@ export class WalletsService {
     return `This action returns a #${id} wallet`;
   }
 
-  update(id: number, updateWalletDto: UpdateWalletDto) {
-    return `This action updates a #${id} wallet`;
+  async findOneByIdOrFail(id: string) {
+    return await this.walletRepository
+      .findOneOrFail({
+        where: { id },
+      })
+      .catch((err) => {
+        throw new BadRequestException('Error wallet not found');
+      });
+  }
+
+  async update(id: string, updateWalletDto: UpdateWalletDto) {
+    await this.walletRepository.update(id, updateWalletDto).catch((err) => {
+      throw new BadRequestException('Error updating wallet');
+    });
+    return await this.findOneByIdOrFail(id);
   }
 
   remove(id: number) {
