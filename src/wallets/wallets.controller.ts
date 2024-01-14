0afc152ea07e42011ff99c4715de70f6673f1e25
alikashmar8 <alikashmar8@gmail.com>
2024-01-14 19:53:32 +0200

    Add roles and guards to WalletsController

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,8 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { EmployeeRole } from 'src/common/enums/employee-role.enum';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
+@ApiBearerAuth('access_token')
 @Controller('wallets')
 export class WalletsController {
   constructor(private readonly walletsService: WalletsService) {}
@@ -22,9 +36,14 @@ export class WalletsController {
     return this.walletsService.findOne(+id);
   }
 
+  @Roles(EmployeeRole.ADMIN)
+  @UseGuards(RolesGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
-    return this.walletsService.update(+id, updateWalletDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateWalletDto: UpdateWalletDto,
+  ) {
+    return await this.walletsService.update(id, updateWalletDto);
   }
 
   @Delete(':id')
--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
-import { WalletsService } from './wallets.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from 'src/app.service';
+import { BranchesService } from 'src/branches/branches.service';
+import { Branch } from 'src/branches/entities/branch.entity';
+import { Chat } from 'src/chats/entities/chat.entity';
+import { Message } from 'src/chats/entities/message.entity';
+import { DeviceToken } from 'src/device-tokens/entities/device-token.entity';
+import { EmployeesService } from 'src/employees/employees.service';
+import { Employee } from 'src/employees/entities/employee.entity';
+import { Setting } from 'src/settings/entities/setting.entity';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { Wallet } from './entities/wallet.entity';
 import { WalletsController } from './wallets.controller';
+import { WalletsService } from './wallets.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Wallet,
+      Message,
+      Employee,
+      DeviceToken,
+      Setting,
+      Chat,
+      Branch,
+      User,
+    ]),
+  ],
   controllers: [WalletsController],
-  providers: [WalletsService]
+  providers: [
+    WalletsService,
+    EmployeesService,
+    AppService,
+    BranchesService,
+    UsersService,
+  ],
 })
 export class WalletsModule {}
